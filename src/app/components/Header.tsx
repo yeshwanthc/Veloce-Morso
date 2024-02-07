c1fa3638462732e2c0029289aa398c6267c16f3c
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2024-02-07 14:12:27 +0530

    Added Header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,38 +1,23 @@
-// Header.tsx
+import Link from "next/link";
+import React from "react";
 
-import React from 'react';
-
-interface HeaderProps {
-  title: string;
-  subtitle: string;
-}
-
-const Header: React.FC<HeaderProps> = ({ title, subtitle }) => {
+const Header = () => {
   return (
-    <header className= "text-black p-6 w-full">
-      <div className="container flex items-center justify-between">
-        {/* Logo (replace 'logo.png' with your actual logo image) */}
-        <img src="#" alt="Logo" className="h-12 w-auto" />
-
-        {/* Navigation Links */}
-        <nav className="space-x-4">
-          <a href="#about" className="text-black hover:underline">
-            About
-          </a>
-          <a href="#projects" className="text-black hover:underline">
-            Projects
-          </a>
-          <a href="#contact" className="text-black hover:underline">
-            Contact
-          </a>
-        </nav>
-      </div>
-
-      {/* Title and Subtitle */}
-      <div className="container mx-auto mt-4">
-        <h1 className="text-3xl font-semibold">{title}</h1>
-        <p className="text-sm">{subtitle}</p>
-      </div>
+    <header className="text-black p-4 w-full flex justify-between items-center bg-[#f1f1f1]">
+      <Link href="/">
+    
+          <img src="/images/VeloceMorso.png" alt="Logo" className="h-[70px] object-cover"/>
+ 
+      </Link>
+      <nav className="flex gap-x-8 font-semibold items-center">
+        <Link className="hover:underline" href="/">Home</Link>
+        <Link className="hover:underline" href="/menu">Menu</Link>
+        <Link className="hover:underline" href="/about">About</Link>
+        <Link className="hover:underline" href="/contact">Contact</Link> 
+        <Link className="hover:underline" href="/cart">Cart</Link>
+        <Link className="bg-red1 py-2 px-3 text-white rounded-[4px] hover:bg-red-600" href="/login">Login</Link>
+      
+      </nav>
     </header>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
 import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <Header title="Yeshwanth" subtitle="Web Developer"/>
-
+    <main className="flex min-h-screen flex-col items-center">
+     <Header />
     </main>
   );
 }
