23e739841dd140f4ecd23fe2a73472e1441dcee2
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2024-02-09 16:10:40 +0530

    added minor things

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section>
+      <h1>With pizza, everything is better.</h1>
+      <p></p>
+    </section>
+  );
+};
+
+export default Hero;
